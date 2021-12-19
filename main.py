139b69e175d16494bdd1738b71ce9140bb320095
User <af2016777@gmail.com>
2021-12-19 18:27:52 +0700

    Добавления кода pygame

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+import pygame
 
+def main():
+    pygame.init()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+main()
