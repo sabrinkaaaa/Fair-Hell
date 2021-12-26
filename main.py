cf7e0346cfb632ef578e39f8697bcd1cec4daf0d
User <af2016777@gmail.com>
2021-12-26 13:26:00 +0700

    Добавил реакцию на кнопки

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    pass
+                elif event.key == pygame.K_DOWN:
+                    pass
+                elif event.key == pygame.K_LEFT:
+                    pass
+                elif event.key == pygame.K_RIGHT:
+                    pass
+                if event.key == pygame.K_z:
+                    pass
+                if event.key == pygame.K_x:
+                    pass
+                if event.key == pygame.K_c:
+                    pass
+                if event.key == pygame.K_v:
+                    pass
+
 
     pygame.quit()
 main()
