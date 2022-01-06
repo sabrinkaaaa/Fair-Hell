75d11c55e5262500444377b1d8046afaa5a82e0e
User <af2016777@gmail.com>
2022-01-06 12:25:12 +0700

    Добавил спрайт для игрока(пока черновой) обьединил классы Jump  и Player

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
-from load_images import load_image
+
+pygame.init()
+abc = pygame.sprite.Group()
+
 
 class Jump:
     def __init__(self):
@@ -23,34 +26,55 @@ class Jump:
         return a, b
 
 
-class Player:
-    def __init__(self, pers):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pers, *group):
+        super().__init__(group)
+        self.image = pygame.image.load('data/mario.png')
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+
         self.pers = pers  # 'argo''peop''priz''diav'
-        self.coords = self.x, self.y = 110, 175
+        self.rect.x, self.rect.y = 110, 525
         # скорость передвижения 10 px
         self.hp = 300
         if self.pers == 'priz':
             self.viz = False
         self.spos = 20
-        self.jump = Jump
+        self.jump = False
 
     def cords(self, x, jump):
-        self.x += x
+        self.rect.x += x
         if jump[0]:
             if jump[1]:
-                self.y += 5
+                self.rect.y += 5
             else:
-                self.y -=5
+                self.rect.y -= 5
 
-    def jump1(self):
-        self.jump.jump_act()
     def attack(self):
         pass
 
+    def jump_act(self):
+        self.jump = -50
+
+    def jump1(self):
+        a = 0
+        b = 0
+        if self.jump < 50:
+            self.jump += 5
+            a = True
+        else:
+            a = False
+        if self.jump < 0:
+            b = True
+        else:
+            b = False
+        return a, b
+
+
+a = Player('priz', abc)
 
 
 def main():
-    pygame.init()
     size = width, height = 1100, 700
     screen = pygame.display.set_mode(size)
     running = True
@@ -60,7 +84,7 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP or event.key == pygame.K_w:
-                    pass
+                    a.jump_act()
                 elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     pass
                 elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
@@ -76,6 +100,9 @@ def main():
                 if event.key == pygame.K_c:
                     # блок(90% урона будет блокировано)
                     pass
+        screen.fill(pygame.Color("BLACK"))
+        abc.draw(screen)
+        pygame.display.flip()
     pygame.quit()
 
 
