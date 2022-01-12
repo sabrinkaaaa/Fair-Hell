1cae8fd86a7bb26a334a44b5a83ccd11b4580aa7
R Sabrina <sabrinaromanenko@gmail.com>
2022-01-12 23:06:03 +0700

    Меню готово

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pygame
 import sys
 import random
+import pickle
 
 width = 1100
 height = 700
@@ -9,6 +10,30 @@ flag = 0
 all_sprites_cloud = pygame.sprite.Group()
 all_sprites_menu = pygame.sprite.Group()
 running = True
+f = 0
+
+
+# сохранение заметки
+def save_file():
+    global flag
+    # ключ
+    date_save = {"save": flag}
+    # создание txt файла
+    with open("conf.txt", "wb") as dump_out:
+        pickle.dump(date_save, dump_out)
+
+
+# распаковка предыдущей заметки
+def read_file():
+    global flag
+    # если txt файл не был создан
+    try:
+        # распаковка txt файла
+        with open('conf.txt', 'rb') as dump_in:
+            date_load = pickle.load(dump_in)
+            flag = date_load["save"]
+    except:
+        print("Нет")
 
 
 def load_image(name, colorkey=None):
@@ -22,6 +47,7 @@ def load_image(name, colorkey=None):
 
 
 class New_game(pygame.sprite.Sprite):
+    global running
     image = load_image("new_game.png")
     image = pygame.transform.scale(image, (350, 200))
 
@@ -41,9 +67,11 @@ class New_game(pygame.sprite.Sprite):
                 self.rect.collidepoint(args[0].pos):
             self.rect.center = (550, 550)
 
-    def new_update(self):
-        self.rect.center = (550, 350)
-        pygame.display.update()
+
+    def new_update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(args[0].pos):
+            self.rect.center = (0, 0)
 
 
 class Continue(pygame.sprite.Sprite):
@@ -67,7 +95,7 @@ class Continue(pygame.sprite.Sprite):
             self.rect.center = (-550, -150)
 
     def new_update(self):
-        self.rect.center = (550, 350)
+        self.rect.center = (-550, -150)
         pygame.display.update()
 
 
@@ -95,6 +123,7 @@ class Exite(pygame.sprite.Sprite):
 
 
 class Cloud(pygame.sprite.Sprite):
+    global f
     image = load_image("cloud1.png")
 
     def __init__(self, *group):
@@ -106,17 +135,21 @@ class Cloud(pygame.sprite.Sprite):
         self.speedx = 0
 
     def update(self):
-        self.rect = self.rect.move(random.randrange(3) - 1,
-                                   random.randrange(3) - 1)
+        if f == 0:
+            self.rect = self.rect.move(random.randrange(3) - 1,
+                                       random.randrange(3) - 1)
+        elif f > 0:
+            self.rect = self.rect.move(random.randrange(50) - 1,
+                                       random.randrange(50) - 1)
 
     def new_update(self):
-        while self.rect.x <= 1100:
-            self.rect = self.rect.move(random.randrange(10) - 1,
-                                       random.randrange(10) - 1)
-            pygame.display.update()
+        self.rect = self.rect.move(random.randrange(50) - 1,
+                                   random.randrange(50) - 1)
+        pygame.display.update()
 
 
 class Cloud_2(pygame.sprite.Sprite):
+    global f
     image = load_image("cloud4.png")
 
     def __init__(self, *group):
@@ -128,19 +161,27 @@ class Cloud_2(pygame.sprite.Sprite):
         self.speedx = 0
 
     def update(self):
-        self.rect = self.rect.move(random.randrange(3) - 1,
-                                   random.randrange(3) - 1)
+        if f == 0:
+            self.rect = self.rect.move(random.randrange(3) - 1,
+                                       random.randrange(3) - 1)
+        elif f > 0:
+            self.rect = self.rect.move(random.randrange(50) - 1,
+                                       random.randrange(50) - 1)
 
     def new_update(self):
-        while self.rect.x <= 1100:
-            self.rect = self.rect.move(random.randrange(10) - 1,
-                                       random.randrange(10) - 1)
-            pygame.display.update()
+        self.rect = self.rect.move(random.randrange(50) - 1,
+                                    random.randrange(50) - 1)
+        pygame.display.update()
 
 
 def main():
+    ex = Exite(pygame.sprite.Group())
+    new_game = New_game(pygame.sprite.Group())
+    cloud_2 = Cloud_2(pygame.sprite.Group())
+    cloud_1 = Cloud(pygame.sprite.Group())
     pygame.init()
     global running
+    global f
 
     # вставляем фон
     screen = pygame.display.set_mode((width, height))
@@ -185,6 +226,9 @@ def main():
         all_sprites_menu.draw(screen)
         all_sprites_menu.update()
         # screen.blit(text, place)
+        #pygame.draw.rect(screen, (255, 255, 255, 127), (375, 450, 350, 200), 0)
+        #pygame.draw.rect(screen, (0, 0, 255), (375, 250, 350, 200))
+        #pygame.draw.rect(screen, (255, 0, 0), (375, 50, 350, 200))
 
         """for i in pygame.event.get():
             if i.type == pygame.QUIT:
@@ -193,10 +237,16 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                if 375 < event.pos[0] < 725 and 450 < event.pos[1] < 650:
+                    ex.update(event)
                 #print("Сука, у  меня эта хуита пол ёбаного дня не работала")
-                all_sprites_menu.update(event)
+                elif 375 < event.pos[0] < 725 and 250 < event.pos[1] < 450:
+                    running = False
+                elif 375 < event.pos[0] < 725 and 50 < event.pos[1] < 250:
+                    running = False
 
-            if event.type == pygame.KEYDOWN:
+
+            """if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     pass
                 elif event.key == pygame.K_DOWN:
@@ -212,11 +262,31 @@ def main():
                 if event.key == pygame.K_c:
                     pass
                 if event.key == pygame.K_v:
-                    pass
+                    pass"""
 
         pygame.display.update()
 
         pygame.time.delay(30)
+    print("hhhhhhhhh")
+    f += 1
+    running = True
+    while running:
+        # ЗДЕСЬ ПОЯВЛЯЛИСЬ ВСЕ ОБЪЕКТЫ
+        screen.blit(bg_surf, bg_rect)
+
+        all_sprites_cloud.update()
+        all_sprites_cloud.draw(screen)
+
+        cloud_2.new_update()
+        cloud_1.new_update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.update()
+
+        pygame.time.delay(30)
+
 
     pygame.quit()
 
